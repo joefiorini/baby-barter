a4b16fd41ce4a25e4caef576d24606e9a96d1148
Joe Fiorini <joe@joefiorini.com>
2013-09-10 02:55:52 +0000

    Redirect to families.index after saving

--- app/controllers/families/new.js
+++ app/controllers/families/new.js
@@ -1,6 +1,9 @@
 var NewController = Ember.ObjectController.extend({
   signUp: function() {
-    this.get("model").save();
+    var self = this;
+    this.get("model").save().then(function() {
+      self.transitionToRoute("families.index");
+    }).fail(Ember.RSVP.rethrow);
   }
 });
 
