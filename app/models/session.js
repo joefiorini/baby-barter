d7b6bba3f6d870c14a6fb97743443b4e6065e95e
Joe Fiorini <joe@joefiorini.com>
2013-09-16 12:46:28 +0000

    Ensure currentUser is always a family object

--- app/models/session.js
+++ app/models/session.js
@@ -1,5 +1,18 @@
 var attr = DS.attr;
 
+function extractUser(store, container, payload) {
+  var raw,
+      factory = container.lookupFactory('model:family'),
+      serializer = store.serializerFor("family");
+
+  payload = {
+    family: payload.user
+  };
+
+  raw = serializer.extract(store, factory, payload, payload.family.id, 'find');
+  return store.push("family", raw);
+}
+
 var Session = Ember.Object.extend({
   email: null,
   password: null,
@@ -10,9 +23,10 @@ var Session = Ember.Object.extend({
   },
   save: function() {
     var self = this;
-    var store = this.get("container").lookup("store:main");
-    var adapter = store.adapterForType("family")
     return new Ember.RSVP.Promise(function(resolve, reject) {
+      var store = self.get("container").lookup("store:main");
+      var adapter = store.adapterForType("family")
+
       var xhr = $.ajax({
         url: adapter.host + "/sessions",
         type: "POST",
@@ -20,11 +34,16 @@ var Session = Ember.Object.extend({
         data: JSON.stringify(self)
       });
       xhr.then(function(result) {
+        var user;
+
         adapter.headers = adapter.headers || {};
         adapter.headers["X-Authorization"] = result.token;
         adapter.headers["X-SignedInAs"] = self.get("email");
+
+        user = extractUser(store, self.get("container"), result);
+
         self.set("token", result.token);
-        self.set("user", result.user);
+        self.set("user", user);
 
         self.cacheCredentials();
 
