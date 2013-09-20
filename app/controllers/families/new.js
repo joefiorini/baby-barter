ebc750e588983674a3acfe67440c7f9e980f0f7b
Joe Fiorini <joe@joefiorini.com>
2013-09-20 13:52:13 +0000

    Sign user in after signing up

--- app/controllers/families/new.js
+++ app/controllers/families/new.js
@@ -1,8 +1,29 @@
+import Session from 'appkit/models/session';
+
 var NewController = Ember.ObjectController.extend({
+  init: function() {
+    this.set("newSession", Session.create({ container: this.container }));
+    this._super();
+  },
+  emailDidChange: function() {
+    this.set("newSession.email", this.get("email"));
+  }.observes("email"),
+  passwordDidChange: function() {
+    this.set("newSession.password", this.get("password"));
+  }.observes("password"),
   signUp: function() {
     var self = this;
+    var password = this.get("password");
     this.get("model").save().then(function() {
-      self.transitionToRoute("families.index");
+      self.set("newSession.password", password);
+      var complete = self.get("newSession").save();
+      complete.then(function(data) {
+        self.target.send("userDidChange", data);
+        self.transitionToRoute("families.index");
+      }, function(error) {
+        self.set("errorCode", error.status);
+        Ember.RSVP.rethrow(error);
+      });
     }).fail(Ember.RSVP.rethrow);
   }
 });
